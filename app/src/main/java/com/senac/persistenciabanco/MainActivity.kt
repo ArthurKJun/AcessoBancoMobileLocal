3a0793634ca253cf118687893760a2f988b9128d
ARTHUR KLUG JUNIOR <arthur.junior@alunos.sc.senac.br>
2024-06-17 20:01:02 -0300

    criado o banco e classe dao, criado tela para interação

--- app/src/main/java/com/senac/persistenciabanco/MainActivity.kt
+++ app/src/main/java/com/senac/persistenciabanco/MainActivity.kt
@@ -3,16 +3,26 @@ package com.senac.persistenciabanco
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.senac.persistenciabanco.ui.theme.PersistenciaBancoTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +32,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
     }
 }
-
+@ExperimentalMaterial3Api
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp(){
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Room DataBase") })
+        }
+    ) {
+        Column (
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PersistenciaBancoTheme {
-        Greeting("Android")
+            modifier = Modifier
+                .padding(it)
+                .fillMaxWidth()
+                .padding(16.dp)
+
+        ){
+            OutlinedTextField(value = "", onValueChange = {}, label = { Text(text = "Name")})
+            OutlinedTextField(value = "", onValueChange = {}, label = { Text(text = "Description")})
+            OutlinedTextField(value = "", onValueChange = {}, label = { Text(text = "Price")})
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Save")
+            }
+        }
     }
 }
--- app/src/main/java/com/senac/persistenciabanco/db/AppDataBase.kt
+++ app/src/main/java/com/senac/persistenciabanco/db/AppDataBase.kt
@@ -1,13 +1,30 @@
 package com.senac.persistenciabanco.db
 
+import android.app.Application
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.senac.persistenciabanco.dao.ProductDao
 import com.senac.persistenciabanco.entities.Product
 
 @Database(entities = [Product::class], version = 1, exportSchema = false)
-abstract class AppDataBase : RoomDatabase(){
+abstract class AppDataBase : RoomDatabase() {
 
-    abstract val productDao : ProductDao
+    abstract val productDao: ProductDao
+
+    //companion é para instanciar apenas 1 vez o banco nao fazer varios acessos
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDataBase? = null
+        fun getDatabase(application: Application): AppDataBase = INSTANCE ?: synchronized(this) {
+            val instance = Room.databaseBuilder(
+                application,
+                AppDataBase::class.java,
+                "product-db"
+            ).build()
+            INSTANCE = instance
+            instance
+        }
+    }
 
 }
